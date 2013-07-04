64f2aee06e3cf44f6d279e05decc4d45c3d15fd2
Martin Podval <martin@podval.eu>
2013-07-04 09:30:01 +0200

    + array topic

---
+++ Java+Scala/Learning/src/ArrayTopic.scala
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Martin
+ * Date: 7/3/13
+ * Time: 4:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ArrayTopic {
+  def method() {
+    var array: scala.Array[Int] = Array(1, 2, 3, 4, 66, 7)
+    println(array)
+  }
+}
